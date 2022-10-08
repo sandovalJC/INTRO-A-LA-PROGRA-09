37c91f1a1d38301920e5419738de809e9afe01a8
sandovalJC <111195084+sandovalJC@users.noreply.github.com>
2022-10-08 11:49:15 -0600

    sesion 9

---
+++ LABORATORIO/sesion 9/P12_JCSA_1020022/P12_JCSA_1020022/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace P12_JCSA_1020022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(373, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mostrar Fibonacci";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingresar un numero mayot a N";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Serie de Fibonacci";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(240, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(243, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 228);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Laboratorio No.7 - 1020022";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ LABORATORIO/sesion 9/P12_JCSA_1020022/P12_JCSA_1020022/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P12_JCSA_1020022
+{
+    public partial class Form1 : Form
+    {
+        int N;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int A = 0;
+            int B = 0;  
+            int C = 0;
+            int i = 0;
+            string resultado = "";
+            try
+            {
+                //lectura del numero
+                N = Convert.ToInt32(textBox1.Text);
+                //serie de fibonacci
+                A = 0;
+                B = 1;
+                C = 0;
+                i = 2;
+                resultado = "";
+                if (N>0)
+                {
+                    //si
+                    resultado = Convert.ToString(A);
+                    if (N > 1)
+                    {
+                        //si
+                        resultado = resultado +", "+ Convert.ToString(B);
+                            while (i < N)
+                            {
+                                C = A + B;
+                                resultado = resultado + ", " + Convert.ToString(C);
+                                A = B;
+                                B = C;
+                                i = i + 1;
+                            }
+                        label3.Text = resultado;
+                    }
+                        else
+                        {
+                            //no
+                            label3.Text = resultado;
+                        }
+                    }
+                    else
+                    {
+                        //no
+                        label3.Text = resultado;
+                    }
+
+                
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Debe ingresar un numero entero", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //Console.WriteLine(err);
+            }
+        }
+    }
+}
