b6c7d9411d99483ece9abc2ebc5afcd1c5e75a2a
sandovalJC <111195084+sandovalJC@users.noreply.github.com>
2023-02-25 09:58:01 -0600

    Add files via upload

---
+++ sesion 5/L4_JCSA_1020022/L4_JCSA_1020022/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4_JCSA_1020022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ejercicio.1 operaciones aritmeticas");
+
+            Console.WriteLine("ingrese numero A");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese numero B");
+            int B = Convert.ToInt32(Console.ReadLine());
+            int suma = (A + B);
+            Console.Write("la suma de " + A);
+            Console.Write("+" + B);
+            Console.Write("=" + suma);
+            Console.ReadLine();
+            int resta = (A - B);
+            Console.Write("la resta de " + A);
+            Console.Write("-" + B);
+            Console.Write("=" + resta);
+            Console.ReadLine();
+            int multip = (A * B);
+            Console.Write("la multiplicacion de " + A);
+            Console.Write("*" + B);
+            Console.Write("=" + multip);
+            Console.ReadLine();
+            int divid = (A / B);
+            Console.Write("la divicion entre " + A);
+            Console.Write("/" + B);
+            Console.Write("=" + divid);
+            Console.ReadLine();
+            int resid = (A % B);
+            Console.Write("el residuo de la divicion entre " + A);
+            Console.Write("/" + B);
+            Console.Write("=" + resid);
+            Console.WriteLine();
+
+            Console.WriteLine("Ejercicio.2: operaciones booleanas");
+            if (A < B)
+            {
+                Console.WriteLine(A + " es menor a " + B);
+            }
+            if (A == B)
+            {
+                Console.WriteLine(A + " es igual a " + B);
+            }
+            if( A > B) 
+            { 
+                Console.WriteLine(A + " es mayor a " + B); 
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Ejercicio.3: Jerarquía de operaciones");
+            Console.WriteLine("ingrese numero A");
+            int A2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese numero B");
+            int B2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese numero C");
+            int C2 = Convert.ToInt32(Console.ReadLine());
+            int operacon1 = (A2 * B2)+C2;
+            int operacon2 = A2 *( B2 + C2);
+            int operacon3 = A2/ (B2 * C2);
+            int operacon4 = ((3*A2)+(2*B2))/(C2*C2);
+            Console.WriteLine("el resultado de la operacion 1 es " + operacon1);
+            Console.WriteLine("el resultado de la operacion 2 es " + operacon2);
+            Console.WriteLine("el resultado de la operacion 3 es " + operacon3);
+            Console.WriteLine("el resultado de la operacion 4 es " + operacon4);
+
+            double a, b, c, discriminante, x1;
+            double x2;
+            Console.Write("Ingresa el valor de a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Ingresa el valor de b: ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Ingresa el valor de c: ");
+            c = double.Parse(Console.ReadLine());
+            discriminante = b * b - 4.0 * a * c;
+            if (discriminante < 0)
+            {
+                discriminante = -discriminante;
+                Console.WriteLine("Soluciones imaginarias");
+            }
+            else
+                Console.WriteLine("Soluciones reales");
+            if (a != 0)
+            {
+                x1 = (-b + Math.Sqrt(discriminante)) / 2.0 / a;
+                x2 = (-b - Math.Sqrt(discriminante)) / 2.0 / a;
+            }
+            else
+            {
+                x1 = 0;
+                x2 = 0;
+                Console.WriteLine("No es una ecuaci\u00F3n cuadr\u00E1tica");
+            }
+            Console.WriteLine("Valor de discriminante: " + discriminante);
+            Console.WriteLine("Valor de x1: " + x1);
+            Console.WriteLine("Valor de x2: " + x2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sesion 5/T4_JCSA_1020022/T4_JCSA_1020022/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4_JCSA_1020022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double Velocidadfin, Velocidadini, aceleraion, tiempo, operacion,respuesta;
+            Console.WriteLine("menu de opciones");
+            Console.WriteLine("opcion 1 = Velocidad Final");
+            Console.WriteLine("opcion 2 = Velocidad Inicial");
+            Console.WriteLine("opcion 3 = Aceleracion");
+            Console.WriteLine("opcion 4 = Tiempo");
+            Console.WriteLine("Ejemplo: si desea calcular Velocidad Final escriba a continuacion solamente numero 1");
+            Console.WriteLine("seleccione la opcion que desea calcular, solo aceptar numero de opcion");
+            operacion = Convert.ToDouble(Console.ReadLine());
+            if (operacion == 1 )
+            {
+                Console.WriteLine("ingrese Velocidad inicial");
+                Velocidadini = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("ingrese Aceleracion");
+                aceleraion = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("ingrese Tiempo");
+                tiempo = Convert.ToDouble( Console.ReadLine());
+                respuesta = Velocidadini + (aceleraion * tiempo);
+                Console.WriteLine("La velocidad final es : " + respuesta);
+                Console.WriteLine();
+            }
+
+            if (operacion == 2)
+            {
+                Console.WriteLine("ingrese Velocidad Final");
+                Velocidadfin = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("ingrese Aceleracion");
+                aceleraion = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("ingrese Tiempo");
+                tiempo = Convert.ToDouble(Console.ReadLine());
+                respuesta = Velocidadfin - (aceleraion*tiempo);
+                Console.WriteLine("La velocidad inicial es : " + respuesta);
+                Console.WriteLine();
+            }
+            if (operacion == 3)
+            {
+                Console.WriteLine("ingrese Velocidad Inicial");
+                Velocidadini = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("ingrese Velocidad Final");
+                Velocidadfin = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("ingrese Tiempo");
+                tiempo = Convert.ToDouble(Console.ReadLine());
+                respuesta = (Velocidadfin-Velocidadini)/tiempo;
+                Console.WriteLine("La aceleracion es : " + respuesta);
+                Console.WriteLine();
+            }
+            if (operacion == 4)
+            {
+                Console.WriteLine("ingrese Velocidad inicial");
+                Velocidadini = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("ingrese Aceleracion");
+                aceleraion = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("ingrese Velocidad Final");
+                Velocidadfin = Convert.ToDouble(Console.ReadLine());
+                respuesta = (Velocidadfin - Velocidadini) / aceleraion;
+                Console.WriteLine("El tiempo es : " + respuesta);
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Gracias por usar el programa :)");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Tarea 4 problema 2 ");
+            Console.Write("Ingrese la cantidad en quetzales (entre 0 y 999.99): ");
+            double cantidad = double.Parse(Console.ReadLine());
+            int billetes100 = (int)cantidad / 100;
+            cantidad %= 100;
+            int billetes50 = (int)cantidad / 50;
+            cantidad %= 50;
+            int billetes20 = (int)cantidad / 20;
+            cantidad %= 20;
+            int billetes10 = (int)cantidad / 10;
+            cantidad %= 10;
+            int billetes5 = (int)cantidad / 5;
+            cantidad %= 5;
+            int monedas1 = (int)cantidad / 1;
+            cantidad %= 1;
+            int monedas25 = (int)(cantidad / 0.25);
+            cantidad %= 0.25;
+            int monedas1c = (int)(cantidad / 0.01);
+            Console.WriteLine();
+            Console.WriteLine("Cantidad de billetes y monedas correspondientes:");
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine($"Billetes de Q100: {billetes100}");
+            Console.WriteLine($"Billetes de Q50: {billetes50}");
+            Console.WriteLine($"Billetes de Q20: {billetes20}");
+            Console.WriteLine($"Billetes de Q10: {billetes10}");
+            Console.WriteLine($"Billetes de Q5: {billetes5}");
+            Console.WriteLine($"Monedas de Q1: {monedas1}");
+            Console.WriteLine($"Monedas de 25 centavos: {monedas25}");
+            Console.WriteLine($"Monedas de 1 centavo: {monedas1c}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
